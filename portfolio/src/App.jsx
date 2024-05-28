a606b71a4f55d911ed9bffbeabb9f6dfb0b52539
elvirkabashi <74553516+elvirkabashi@users.noreply.github.com>
2024-05-28 23:44:02 +0200

    Parallax effect

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,18 @@
 import "./app.scss";
 import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
+import Parallax from "./components/parallax/Parallax";
 
 function App() {
 
   return (
     <>
+    <section id="Homepage" className="section">
       <Navbar/>
       <Hero/>
+    </section>
+    <section id="Services" className="section"><Parallax type="services"/></section>
+    <section id="Portfolio" className="section"><Parallax type="portfolio"/></section>
     </>
   )
 }
